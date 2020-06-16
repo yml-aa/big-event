97ce4d1139e3dea06cc5bb4935f455053e00bcc4
你的猫 <2697580582@qq.com>
2020-06-16 22:59:38 +0800

    注册功能基本完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,16 +9,31 @@ $(function() {
             "用户名必须6到8位，且不能出现空格"
         ],
         pwd : [
-            /^[\d]{5}$/,
-            "密码必须6到8位，且不能出现空格"
-        ]
+            /^[a-zA-Z0-9]{5}$/,
+            "密码必须5位，且不能出现空格"
+        ],
+        samePwd : function(value) {
+            var code = $('#password').val()
+            // 判断
+            if(value != code) {
+                return '两次的密码不一致'
+            }
+        }
+    })
+    // 点击去注册账号按钮
+    $('#form-reg1 a').click(function(e) {
+        // 阻止默认行为
+        e.preventDefault()
+        $('#form-reg1').hide()
+        $('#form-reg2').show()
     })
     // 获取输入框的内容
-    $('.layui-form').submit(function(e) {
+    $('#form-reg1').submit(function(e) {
         // 事件对象 阻止默认行为
         e.preventDefault()
         // 快速获取输入框内容
         var formData = $(this).serialize()
+        // console.log(formData);s
         // 将得到的输入框数据发送到后台
         $.ajax({
             type : 'post',
@@ -31,5 +46,33 @@ $(function() {
                 }
             }
         })  
+    })
+
+    // 点击立即提交按钮
+    $('#form-reg2').submit(function(e) {
+        // alert('111');
+        e.preventDefault();
+        // 获取输入框的数据
+        var formData1 = $(this).serialize()  
+        // 将输入框的数据提交到后台
+        $.ajax({
+            type : 'post',
+            url : 'http://ajax.frontend.itheima.net/api/reguser',
+            data : formData1,
+            success : function(res) {
+                if(res.status != 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg('注册成功，请登录！')
+                $('#link-login').click()
+            }
+        })
+    })
+    // 点击去登录按钮
+    $('#form-reg2 a').click(function(e) {
+        // 阻止默认行为
+        e.preventDefault()
+        $('#form-reg2').hide()
+        $('#form-reg1').show()
     })
 })
