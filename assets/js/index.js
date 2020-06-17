7089358ec2e13af43c892c9cf0ecb43c4c99efbb
你的猫 <2697580582@qq.com>
2020-06-17 22:55:34 +0800

    填充用户名和头像

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,4 +20,37 @@ $(function() {
             location.href = './login.html'
         });
     })
+
+    // 加载时需要获取调用后台接口和用户信息
+    function loadUserInfo() {
+        $.ajax({
+            type : 'get',
+            // 请求地址
+            url : 'http://ajax.frontend.itheima.net/my/userinfo',
+            headers : {
+                Authorization:localStorage.getItem('mytoken')
+            },
+            success : function(res) {
+                console.log(res);
+                if(res.status === 0) {
+                    // 获得用户的信息
+                    var userData = res.data
+                    // 将获得得信息填充到相对于的位置
+                    // 填充用户名
+                    $('#wel-username').html(userData.username)
+                    $('#nav-username').html(userData.username)
+                    // 填充头像
+                    // userData.user_pic = 'http://t.cn/RCzsdCq'
+                    // 如果存在头像 显示一张照片
+                    if(userData.user_pic){
+                        $('#wel-username').parent().prev('div').remove()
+                        $('#wel-username').parent().prepend('<img src="'+userData.user_pic+'" alt="" />')
+                        $('#nav-username').parent().prev('div').remove()
+                        $('#nav-username').parent().prepend('<img src="'+userData.user_pic+'" alt="" />')
+                    }
+                }
+            }
+        })
+    }
+    loadUserInfo()
 })
