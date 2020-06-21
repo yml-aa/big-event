964ffd9dea820e098e0e5a16501681d4b12e922a
你的猫 <2697580582@qq.com>
2020-06-21 22:29:00 +0800

    初步文章列表

---
+++ assets/js/article/articleList.js
@@ -0,0 +1,14 @@
+// $(function() {
+//     // 获取后台的数据 渲染到页面上
+//     function articleListData() {
+//         $.ajax({
+//             type: 'get',
+//             url: '/my/article/list',
+//             data: data,
+//             success: function(res) {
+//                 console.log(res);
+//             }
+//         })
+//     }
+//     articleListData()
+// })
